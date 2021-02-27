1aecf469cde95b0d7af08740e1c3627341b4bece
Stanislav Kiselev <stif@tut.by>
2021-02-27 15:02:49 +0300

    refactor: code optimization

--- script.js
+++ script.js
@@ -7,27 +7,16 @@ let memoryCurrentNumber = 0;
 let memoryNewNumber = false;
 let memoryPendingOperation = '';
 
-for (i = 0; i < numbers.length; i++) {
-  let number = numbers[i];
-  number.addEventListener('click', (e) => operandEnter(e.target.textContent));
-}
+numbers.forEach((e, i) => numbers[i].addEventListener('click', (e) => operandEnter(e.target.textContent)));
 
 let operandEnter = (number) => {
   if (memoryNewNumber) {
     display.value = number;
     memoryNewNumber = false;
-  } else {
-    if (display.value === '0') {
-      display.value = number;
-    } else  display.value += number;
-  }
-     console.log("CLICK-OPERAND " + number + ' !');
+  } else display.value === '0' ? display.value = number : display.value += number;
 };
 
-for (i = 0; i < operations.length; i++) {
-  let operationButton = operations[i];
-    operationButton.addEventListener('click', (e) => operatorEnter(e.target.textContent));
-}
+operations.forEach((e, i) => operations[i].addEventListener('click', (e) => operatorEnter(e.target.textContent)));
 
 let operatorEnter = (op) => {
   let localOperationMemory = display.value;
@@ -49,10 +38,7 @@ let operatorEnter = (op) => {
   } 
 };
 
-for (i = 0; i < clearButtons.length; i++) {
-  let clearButton = clearButtons[i];
-  clearButton.addEventListener('click', (e) => clearDisplay(e.srcElement.id));
-}
+clearButtons.forEach((e, i) => clearButtons[i].addEventListener('click', (e) => clearDisplay(e.srcElement.id)));
 
 let clearDisplay = (id) => {
   if (id === 'ce') {
